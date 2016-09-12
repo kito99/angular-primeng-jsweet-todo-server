ab00749059b2676292e7301c235f66e32709f468
kito99 <kito.mann@virtua.com>
2016-09-11 21:39:19 -0400

    Added Weld initialization

--- services/src/main/java/virtua/Main.java
+++ services/src/main/java/virtua/Main.java
@@ -3,6 +3,7 @@ package virtua;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.jboss.weld.environment.se.Weld;
 import virtua.training.primeng.jsweet.todo.TodoApplication;
 
 import java.io.IOException;
@@ -14,20 +15,33 @@ import java.net.URI;
  */
 public class Main {
     // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://localhost:8080/myapp/";
+    private static final URI BASE_URI = URI.create("http://localhost:8080/myapp/");
 
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
      * @return Grizzly HTTP server.
      */
-    public static HttpServer startServer() {
+    public static HttpServer startServer() throws IOException {
+
+        final Weld weld = new Weld();
+        weld.initialize();
+
         // create a resource config that scans for JAX-RS resources and providers
         // in virtua package
-        final ResourceConfig rc = ResourceConfig.forApplicationClass(TodoApplication.class);
+        final ResourceConfig resourceConfig = ResourceConfig.forApplicationClass(TodoApplication.class);
 
         // create and start a new instance of grizzly http server
         // exposing the Jersey application at BASE_URI
-        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+        final HttpServer server = GrizzlyHttpServerFactory.createHttpServer(BASE_URI, resourceConfig, false);
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            @Override
+            public void run() {
+                server.shutdownNow();
+                weld.shutdown();
+            }
+        }));
+        server.start();
+        return server;
     }
 
     /**
@@ -36,7 +50,9 @@ public class Main {
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
+
         final HttpServer server = startServer();
+
         System.out.println(String.format("Jersey app started with WADL available at "
                 + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
         System.in.read();
